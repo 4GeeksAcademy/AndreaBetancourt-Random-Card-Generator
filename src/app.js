811d26706c075a0fa21a082363cce58563772b13
Andrea Betancourt <111385731+AndreaBetan@users.noreply.github.com>
2023-06-27 12:41:56 +0000

    Primera parte

--- src/app.js
+++ src/app.js
@@ -5,16 +5,47 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+window.onload = function () {
   //write your code here
+  cardGenerator(simbols, numbers)
+  
+  //ejecuta la función de generación de cartas inmediatamente al cargar la página y luego se ejecute cada 10 segundos.
+  setInterval(()=>{cardGenerator(simbols,numbers)}, 10000)
 
-  let header = document.querySelector("#header");
-  header.innerHTML = "Hola";
 };
 
-// window.onload = function loadListener() {
-//   alert("loading finished...");
-// };
+let cardGenerator = function (simb, num){
 
-// let load = document.querySelector("body");
-// load.innerHTML = loadListener();
+  let randomSimbols = simb[Math.floor(Math.random() * simb.length)];
+  let randomNumbers = num[Math.floor(Math.random() * num.length)];
+
+  let header = document.querySelector(".cardheader");
+  header.innerHTML = randomSimbols;
+
+  let body = document.querySelector(".cardbody")
+  body.innerHTML = randomNumbers;
+
+  let footer = document.querySelector(".cardfooter");
+  footer.innerHTML = randomSimbols;
+
+  if (randomSimbols === '♥' || randomSimbols === '♦'){
+    header.style.color = "red"; body.style.color = "red"; footer.style.color = "red";
+  }else{
+    header.style.color = "black"; body.style.color = "black"; footer.style.color = "black"
+  };
+
+  document.querySelector("#btn").addEventListener("click", () => {
+    cardGenerator(simbols,numbers);
+  });
+
+};
+
+let simbols = ['♦', '♥', '♠', '♣'];
+let numbers = ['2','3','4','5','6','7','8','9','10','K','Q','J','A'];
+
+
+
+
+
+
+
